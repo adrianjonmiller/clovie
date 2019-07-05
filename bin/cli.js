dce877d0092c9da03b04b501a0d97508031fcd90
Adrian Miller <adrianjonmiller@gmail.com>
2019-07-05 15:19:37 -0700

    moved cli to bin

--- bin/cli.js
+++ bin/cli.js
@@ -2,11 +2,11 @@
 
 // NPM
 const path = require("path");
-const commandLineArgs = require('command-line-args');
+const commandLineArgs = require("command-line-args");
 
 // Local
-const ATX = require('./lib');
-const optionDefinitions = require('./optionDefinitions');
+const ATX = require("../lib");
+const optionDefinitions = require("../optionDefinitions");
 const options = commandLineArgs(optionDefinitions);
 
 // Config path
@@ -19,13 +19,13 @@ const config = require(configPath);
 const site = new ATX(config);
 
 site.error(err => {
-  console.log(err)
+  console.log(err);
 });
 
 if (options.watch) {
   site.build().then(() => {
     site.watch();
-  })
+  });
 } else {
   site.build();
 }
