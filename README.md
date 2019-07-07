2f50faa55bceafacc24147b5f713b0738c68a1dd
Adrian Miller <adrianjonmiller@gmail.com>
2019-07-06 21:37:48 -0700

    Added templaing to nunjucks in demo

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # ATX - extensible static site generator.
 
+#### Installation
+
+```
+yarn add atx
+```
+
+#### app.config.js file
+
+```
+{
+  scripts: path.join('./scripts/index.js'), // Path to script entry file
+  styles: path.join('./styles'), // Path to Sass entry file
+  views: path.join('./views'), // Path to views directory
+  outputDir: path.resolve('./dist/'), // Path to output directory
+  data: {}, // Data goes here, can be either obect or Promise
+  compiler: (template, data) => { return somecompiler.output() } // Retern the result of compiler ex: nunucks, handlebars, liquid etc.
+}
+
+```
+
+#### Build
+
+```
+npm atx
+```
+
+#### Watch
+
+```
+npm atx --watch
+```
+
+or
+
+```
+npm atx -w
+```
--- demo/app.config.js
+++ demo/app.config.js
@@ -3,39 +3,16 @@ const fetch = require('node-fetch');
 const token = '65bd73f5a93d46397f2b027a16d8b1';
 const path = require('path');
 
+nunjucks.configure('./views');
+
 module.exports = {
   scripts: path.join('./scripts/index.js'),
   styles: path.join('./styles/main.scss'),
   views: path.join('./views'),
   outputDir: path.resolve('./dist/'),
-  data: function () {
-    return new Promise (resolve => {
-      fetch('https://graphql.datocms.com/', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'Accept': 'application/json',
-          'Authorization': `Bearer ${token}`,
-        },
-        body: JSON.stringify({
-          query: `{
-            allWords {
-              word
-              example
-            }
-          }`
-        }),
-      }).then(res => res.json()).then((res) => {
-        resolve(res.data)
-      }).catch((error) => {
-        console.log(error);
-      });
-    }) 
-  },
-  models: {
-    'allWords': {
-      template: 'templates/word.html'
-    }
-  },
+  data: () => ({
+    title: 'Attics'
+  }),
+  models: {},
   compiler: (template, data) => (nunjucks.renderString(template, data))
 }
