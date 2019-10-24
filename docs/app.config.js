67a3e1838e2f443761c47005ba9342113cf68c7c
Adrian Miller <adrianjonmiller@gmail.com>
2019-10-24 13:50:48 -0700

    added promise return for data

--- docs/app.config.js
+++ docs/app.config.js
@@ -1,13 +1,47 @@
 const nunjucks = require('nunjucks');
+const fetch = require('node-fetch');
+
+const token = '678a447ea152794270ae8342526551';
 
 nunjucks.configure('./views', {
-  watch: false
+  watch: process.env.NODE_ENV === 'dev'
 });
 
 module.exports = {
-  data: () => ({
-    title: 'Attics'
+  data: new Promise(result => {
+    fetch(
+      'https://graphql.datocms.com/',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+          'Authorization': `Bearer ${token}`,
+        },
+        body: JSON.stringify({
+          query: `{
+            allPhotos {
+              title
+              photo {
+                url
+                alt
+              }
+            }
+          }`
+        }),
+      }
+    )
+    .then(res => res.json())
+    .then(res => {
+      result(res.data)
+    })
+    .catch((error) => {
+      reject(error)
+    })
   }),
   models: {},
-  compiler: (template, data) => (nunjucks.renderString(template, data))
+  compiler: (template, data) => {
+    console.log(data)
+    return nunjucks.renderString(template, data);
+  }
 };
