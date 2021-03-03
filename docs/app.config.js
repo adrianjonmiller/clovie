ea5d1af689c8ac0586841895ed8770e16f1e369b
Adrian Miller <adrianjonmiller@gmail.com>
2021-03-03 08:09:30 -0600

    Separated database and query

--- docs/app.config.js
+++ docs/app.config.js
@@ -1,57 +1,19 @@
 require('dotenv').config();
 const nunjucks = require('nunjucks');
-const fetch = require('node-fetch');
+const path  = require('path');
 const token = process.env.DATO_TOKEN;
+const templatesPath = path.join('./views/nunjucks');
+const data = require('./database')
 
-nunjucks.configure('./views', {
+nunjucks.configure(templatesPath, {
   watch: process.env.NODE_ENV === 'dev'
 });
 
 module.exports = {
-  data: new Promise(result => {
-    fetch(
-      'https://graphql.datocms.com/',
-      {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'Accept': 'application/json',
-          'Authorization': `Bearer ${token}`,
-        },
-        body: JSON.stringify({
-          query: `{
-            allPhotos {
-              title
-              photo {
-                url
-                alt
-              }
-            }
-          }`
-        }),
-      }
-    )
-    .then(res => res.json())
-    .then(res => {
-      result(res.data)
-    })
-    .catch((error) => {
-      reject(error)
-    })
-  }),
-  models: {
-    allPhotos: {
-      template: '_article.html',
-      output: (val) => {
-        return 'articles/' + val.title.toLowerCase() + '.html'
-      },
-      transform: (val) => {
-        return val
-      }
-    }
-  },
+  data: data(token),
+  models: {},
+  views: templatesPath,
   compiler: (template, data) => {
-    console.log(data)
     return nunjucks.renderString(template, data);
   }
 };
