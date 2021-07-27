32c7296ed566935f77f0a2c1daa3c21f9f1a1ec1
Adrian Miller <adrianjonmiller@gmail.com>
2021-07-27 13:34:44 -0500

    add: created ability to paginate data

--- docs/app.config.js
+++ docs/app.config.js
@@ -3,15 +3,21 @@ const nunjucks = require('nunjucks');
 const path  = require('path');
 const token = process.env.DATO_TOKEN;
 const templatesPath = path.join('./views/nunjucks');
-const data = require('./database')
+const data = require('./data')
 
 nunjucks.configure(templatesPath, {
   watch: process.env.NODE_ENV === 'dev'
 });
 
 module.exports = {
-  data: data(token),
-  models: {},
+  data: {
+    test: [1,2,3,4,5]
+  },
+  models: {
+    test: {
+      paginate: 2
+    }
+  },
   views: templatesPath,
   compiler: (template, data) => {
     return nunjucks.renderString(template, data);
